9c145cc84ad378810cbfa7b4c1da0550827b3d77
onigra <3280467rec@gmail.com>
2019-03-03 01:28:06 +0900

    repl list

---
+++ src/repls.ts
@@ -0,0 +1,19 @@
+const Repls = [
+  {
+    id: "Ruby",
+    title: "Irb(Ruby)",
+    command: "exit"
+  },
+  {
+    id: "Node",
+    title: "Node",
+    command: "process.exit();"
+  },
+  {
+    id: "Python",
+    title: "Python",
+    command: "quit"
+  }
+];
+
+export default Repls;
