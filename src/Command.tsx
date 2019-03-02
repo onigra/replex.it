7baaebe49e1981bc3dcaf1c0d01f6e237d365ace
onigra <3280467rec@gmail.com>
2019-03-03 02:00:09 +0900

    rename

--- src/Command.tsx
+++ src/Command.tsx
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 
-interface ReplsProps {
+interface CommandProps {
   id: string;
   title: string;
   command: string;
 }
 
-class Command extends Component<ReplsProps> {
+class Command extends Component<CommandProps> {
   render() {
     return (
       <div id={this.props.id}>
